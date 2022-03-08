7a3d13011f72cee9c074999da9b5c3551976b0b3
BiFoS <jon89071@gmail.com>
2022-03-08 21:09:18 +0900

    coin: pow implementing

--- foscoin/blockchain/chain.go
+++ foscoin/blockchain/chain.go
@@ -7,9 +7,17 @@ import (
 	"github.com/jon890/foscoin/utils"
 )
 
+const (
+	defaultDifficulty  int = 2
+	difficultyInterval int = 5
+	blockInterval      int = 2
+	allowedRange       int = 2
+)
+
 type blockchain struct {
-	NewestHash string `json:"newestHash"`
-	Height     int    `json:"height"`
+	NewestHash        string `json:"newestHash"`
+	Height            int    `json:"height"`
+	CurrentDifficulty int    `json:"currentDifficulty"`
 }
 
 var b *blockchain
@@ -27,6 +35,7 @@ func (b *blockchain) AddBlock(data string) {
 	block := createBlock(data, b.NewestHash, b.Height+1)
 	b.NewestHash = block.Hash
 	b.Height = block.Height
+	b.CurrentDifficulty = block.Difficulty
 	b.persist()
 }
 
@@ -46,10 +55,36 @@ func (b *blockchain) Blocks() []*Block {
 	return blocks
 }
 
+func (b *blockchain) recalculateDifficulty() int {
+	allBlocks := b.Blocks()
+	newestBlock := allBlocks[0]
+	lastRecalculatedBlock := allBlocks[difficultyInterval-1]
+	acutalTime := (newestBlock.Timestamp - lastRecalculatedBlock.Timestamp) / 60
+	expectedTime := difficultyInterval * blockInterval
+
+	if acutalTime < (expectedTime - allowedRange) {
+		return b.CurrentDifficulty + 1
+	} else if acutalTime > (expectedTime + allowedRange) {
+		return b.CurrentDifficulty - 1
+	} else {
+		return b.CurrentDifficulty
+	}
+}
+
+func (b *blockchain) difficulty() int {
+	if b.Height == 0 {
+		return defaultDifficulty
+	} else if b.Height%difficultyInterval == 0 {
+		return b.recalculateDifficulty()
+	} else {
+		return b.CurrentDifficulty
+	}
+}
+
 func Blockchain() *blockchain {
 	if b == nil {
 		once.Do(func() {
-			b = &blockchain{"", 0}
+			b = &blockchain{Height: 0}
 
 			checkpoint := db.CheckPoint()
 			if checkpoint == nil {
