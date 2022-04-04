850e2c273d0b7d59191188f81d526738fc53b0a0
BiFoS <jon89071@gmail.com>
2022-04-04 16:39:09 +0900

    walle: restore sign and verifying

--- foscoin/wallet/wallet.go
+++ foscoin/wallet/wallet.go
@@ -1,33 +1,40 @@
 package wallet
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
+	"crypto/x509"
 	"encoding/hex"
 	"fmt"
+	"math/big"
 
 	"github.com/jon890/foscoin/utils"
 )
 
+const (
+	privateKey    = "30770201010420946539b844e42f19ffe8b78724d7fb1af462af1841b7f8591dce1ffffcacd758a00a06082a8648ce3d030107a1440342000409415138a382d8dd45bb0e0d3fe2249e48eee1951a570f952a0351c9fb452f701b2b3c106338ae26416d651f121499d7c4aea9e85ef93d4e30f57d394a4a4880"
+	hashedMessage = "a68c33f6b0eed19a07a21ae8e576ae570dafa489df93b8e13a92d0ea2ca43a2c"
+	signature     = "65dc77d184cf3fe9dfda92433f8cf3c8cfbef666a43258795db2dff91d952ac8da3c67041ff442efeb4bfa3096d48a222ef0adcf51d7a9dba00b9219cbb4cbf5"
+)
+
 func Start() {
-	// 1. Generate Keypair
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	utils.HandleErr(err)
-	// fmt.Println("Private Key", privateKey.D)
-	// fmt.Println("Public Key, x, y", privateKey.X, privateKey.Y)
-
-	// 2. Hash message
-	message := "I am hungry"
-	hashedMessage := utils.Hash(message)
-	hashAsBytes, err := hex.DecodeString(hashedMessage)
+	privateKeyAsByte, err := hex.DecodeString(privateKey)
 	utils.HandleErr(err)
 
-	// 3. Sign message
-	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
+	restoredKey, err := x509.ParseECPrivateKey(privateKeyAsByte)
 	utils.HandleErr(err)
-	//fmt.Printf("R:%d\nS:%d\n", r, s)
 
-	ok := ecdsa.Verify(&privateKey.PublicKey, hashAsBytes, r, s)
-	fmt.Println(ok)
+	fmt.Println(restoredKey)
+
+	signBytes, err := hex.DecodeString(signature)
+	utils.HandleErr(err)
+
+	rBytes := signBytes[:len(signBytes)/2]
+	sBytes := signBytes[len(signBytes)/2:]
+
+	fmt.Println(signBytes, rBytes, sBytes)
+
+	var bigR, bigS = big.Int{}, big.Int{}
+	bigR.SetBytes(rBytes)
+	bigS.SetBytes(sBytes)
+
+	fmt.Println(bigR, bigS)
 }
