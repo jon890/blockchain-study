39615a0a67b41804566d4dd5f1806eea089eba0f
BiFoS <jon89071@gmail.com>
2022-04-11 20:16:57 +0900

    p2p: refactor

--- foscoin/p2p/p2p.go
+++ foscoin/p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/jon890/foscoin/utils"
@@ -13,19 +12,21 @@ var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	// Port :3000 will upgrade the request from :4000
+	openPort := r.URL.Query().Get("openPort")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
+
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		return openPort != "" && ip != ""
 	}
+
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	openPort := r.URL.Query().Get("openPort")
-	result := strings.Split(r.RemoteAddr, ":")
-	initPeer(conn, result[0], openPort) // TODO
+	initPeer(conn, ip, openPort) // TODO
 }
 
 func AddPeer(address, port, openPort string) {
 	// from :4000 is requesting an upgrade from the port :3000
-	urlStr := fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort)
+	urlStr := fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:])
 	conn, _, err := websocket.DefaultDialer.Dial(urlStr, nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
